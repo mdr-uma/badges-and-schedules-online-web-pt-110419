1ae136c94e5f736d1ba6be495b7e11eaab576a62
Uma <um_2003_8@hotmail.com>
2020-10-09 18:07:20 -0400

    Update conference_badges.rb

--- conference_badges.rb
+++ conference_badges.rb
@@ -27,8 +27,8 @@ def printer(attendees)
   batch_badge_creator(attendees).each do |badge|
     puts badge
   end
-  assign_rooms(attendees).each do |badge|
-    puts badge
+  assign_rooms(attendees).each do |assignment|
+    puts assignment
   end
 
 end
